37bceb3c540c07e06dfc5739297e2ab5187cd5e5
khalyanakartika <khalyanakartika@gmail.com>
2025-03-27 22:48:47 +0700

    initial commit & add class Karyawan

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public abstract class Karyawan
+{
+    private string Nama;
+    private string Id;
+    private double Gajipokok;
+
+    public Karyawan(string Nama, string Id, double GajiPokok)
+    {
+        this.Nama = Nama;
+        this.Id = Id;
+        this.Gajipokok = GajiPokok;
+    }
+
+    public string nama
+    {
+        get { return Nama; }
+        set { Nama = value; }
+    }
+
+    public string id
+    { 
+        get { return Id; } 
+        set { Id = value; }
+    }
+
+    public double gajipokok
+    {
+        get { return Gajipokok; }
+        set { Gajipokok = value; }
+    }
+
+    public abstract double HitungGaji();
+}
